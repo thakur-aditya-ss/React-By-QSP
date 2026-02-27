5351b45d39309f2c7691318e98cf3bfe16a358c2
Aditya Singh <aaasingh1010@gmail.com>
2026-02-27 21:58:26 +0530

    updated

---
+++ src/AllTopics/01_TypesOfComponents/ClassBasedComponents.jsx
@@ -0,0 +1,13 @@
+
+import { Component } from "react";
+
+class ClassBased extends Component{
+    render(){
+        return(
+                <>
+                    <h1>I Am ClassBased Component</h1>
+                </>
+        );
+    };
+};
+export default ClassBased;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import HeaderComp from "./components/HeaderComp";
+// import HeaderComp from "./components/HeaderComp";
+// const App = () => {
+//   let greet = "Welcome";
+//   return (
+//     <>
+//       <h1>I Am Parent Component</h1>
+//       <h2>{greet}</h2>  
+//        {/* yaha par {} Expression hai  */}
+
+//        <HeaderComp/>
+//     </>
+//   );
+// };
+// export default App;
+
+//! belongs to FunctionBased and ClassBased and StatesInFBC and so on.
+import ClassBased from "./AllTopics/01_TypesOfComponents/ClassBasedComponents";
+import FunctionBased from "./AllTopics/01_TypesOfComponents/FunctionBased";
+
 
 const App = () => {
-  let greet = "Welcome";
+
   return (
     <>
-      <h1>I Am Parent Component</h1>
-      <h2>{greet}</h2>  
-       {/* yaha par {} Expression hai  */}
-
-       <HeaderComp/>
+        <FunctionBased/>
+        <ClassBased/>
     </>
   );
 };
 export default App;
-
