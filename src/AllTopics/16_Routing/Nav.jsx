d4c58ce7a19b74ab79305dd5aec3c1e57c57bb4d
Aditya Singh <aaasingh1010@gmail.com>
2026-03-20 12:32:05 +0530

    routing Nav

--- src/AllTopics/16_Routing/Nav.jsx
+++ src/AllTopics/16_Routing/Nav.jsx
@@ -15,14 +15,31 @@
 
 // ! Using Routing - React Router DOM
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <header className="p-5 bg-violet-600 text-white font-bold">
-      <nav className="flex gap-10">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <nav className="flex gap-10 ">
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
+
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "bg-white text-black" : ""} transition ease-in py-1 px-3 rounded`
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? "bg-white text-black" : ""} transition ease-in py-1 px-3 rounded`
+          }
+          to="/about"
+        >
+          About
+        </NavLink>
       </nav>
     </header>
   );
